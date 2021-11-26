ec0dfb7c9f37c217ca748f02dcef37e3614d5223
Ayfri <pierre.ayfri@gmail.com>
2021-11-26 16:46:29 +0100

    feat: Inform player that a letter is already submitted.

--- src/hangman.go
+++ src/hangman.go
@@ -15,28 +15,12 @@ const exit = "STOP"
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	selectedFile := os.Args[1]
-	file, err := ioutil.ReadFile(selectedFile)
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			log.Fatal("File does not exist")
-		} else {
-			log.Fatalf("Error reading file : %v", err)
-		}
-	}
-	split := strings.Split(string(file), "\n")
-	word := split[rand.Intn(len(split))]
+	word := chooseWordFromFile(os.Args[1])
 
 	var letters []string
+	var submittedLetters []string
 	lettersToReveal := len(word)/2 - 1
-	for i := 0; i < len(word); i++ {
-		letters = append(letters, "_")
-	}
-
-	for i := 0; i < lettersToReveal; i++ {
-		index := rand.Intn(len(word))
-		letters[index] = string(word[index])
-	}
+	letters = setVisibleLetters(word, letters, lettersToReveal)
 
 	attempts := 10
 	for {
@@ -47,10 +31,17 @@ func main() {
 		}
 
 		if submission == word {
+			win()
 			break
 		}
 
 		if len(submission) == 1 && isLetter(submission) {
+			if isLetterSubmitted(submission, submittedLetters) {
+				fmt.Println("You already submitted this letter")
+				continue
+			}
+
+			submittedLetters = append(submittedLetters, submission)
 			if !isLetterInWord(submission, word) {
 				attempts--
 				fmt.Printf("Wrong submission, attempts: %v\n", attempts)
@@ -66,15 +57,32 @@ func main() {
 			}
 		}
 		if isWordGuessed(letters, word) {
+			win()
 			break
 		}
 	}
-	fmt.Printf("You won!")
+}
+
+func chooseWordFromFile(selectedFile string) string {
+	file, err := ioutil.ReadFile(selectedFile)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			log.Fatal("File does not exist")
+		} else {
+			log.Fatalf("Error reading file : %v", err)
+		}
+	}
+	split := strings.Split(string(file), "\n")
+	return split[rand.Intn(len(split))]
 }
 
 func getLetter() (result string, doExit bool) {
 	letter := strings.ToLower(readLine())
 	return letter, letter == exit
+}
+
+func isLetter(letter string) bool {
+	return letter >= "a" && letter <= "z"
 }
 
 func isLetterInWord(letter string, word string) bool {
@@ -86,8 +94,13 @@ func isLetterInWord(letter string, word string) bool {
     return false
 }
 
-func isLetter(letter string) bool {
-	return letter >= "a" && letter <= "z"
+func isLetterSubmitted(submission string, letter []string) bool {
+	for _, l := range letter {
+		if l == submission {
+			return true
+		}
+	}
+	return false
 }
 
 func isWordGuessed(letters []string, word string) bool {
@@ -99,6 +112,18 @@ func isWordGuessed(letters []string, word string) bool {
 	return true
 }
 
+func setVisibleLetters(word string, letters []string, lettersToReveal int) []string {
+	for i := 0; i < len(word); i++ {
+		letters = append(letters, "_")
+	}
+
+	for i := 0; i < lettersToReveal; i++ {
+		index := rand.Intn(len(word))
+		letters[index] = string(word[index])
+	}
+	return letters
+}
+
 func readLine() string {
 	var line string
 	_, err := fmt.Scanln(&line)
@@ -106,4 +131,8 @@ func readLine() string {
 		log.Fatalf("Error reading line : %v", err)
 	}
 	return line
+}
+
+func win() {
+	fmt.Printf("You won!")
 }
